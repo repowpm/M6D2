69570422100949bc2e086171d1e100b6f1ec4e46
repowpm <wal.paredesm@gmail.com>
2024-10-11 09:07:20 -0300

    Desafio 2 M6

---
+++ src/components/bindingFormulario.vue
@@ -0,0 +1,153 @@
+<template>
+    <div id="app">
+      <form>
+        <div>
+          <label>Título de la tarjeta: </label>
+          <input v-model="titulotarjeta"/>
+        </div>
+
+        <div>
+          <label>Chip SRC: </label>
+          <input type="text" v-model="Chip" readonly />
+        </div>
+
+        <div>
+          <label>Número: </label>
+          <input v-model="numero"/>
+        </div>
+
+        <div>
+          <label>Fecha de expiración: </label>
+          <input v-model="fechadeexpiracion"/>
+        </div>
+
+        <div>
+          <label>Propietario: </label>
+          <input v-model="propietario" />
+        </div>
+
+        <div>
+          <label>Tipo de tarjeta SRC: </label>
+          <input  type="text" v-model="tipotarjeta" readonly/>
+        </div>
+      </form>
+
+      <div class="carnet">
+        <h3>{{titulotarjeta}}</h3>
+        <img width="40" :src="Chip" alt="" />
+        <div class="datos">
+          <h2>{{numero}}</h2>
+          <span>Fecha Exp: <b>{{fechadeexpiracion}}</b></span>
+        </div>
+        <footer>
+          <span>{{propietario}}</span>
+          <img :src="tipotarjeta" width="60" />
+        </footer>
+      </div>
+    </div>
+
+  </template>
+  
+  <script>
+  export default {
+    name: 'bindingFormulario',
+    data(){
+        return{
+            titulotarjeta:"",
+            Chip:"/chip.png",
+            numero:"",
+            fechadeexpiracion:"",
+            propietario:"",
+            tipotarjeta:"/visa.png"
+        };
+    },
+    props: {
+      msg: String
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style>
+  * {
+    margin: 0;
+  }
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 60vh;
+    font-size: 10px;
+    font-family: sans-serif;
+  }
+
+  #app {
+    display: flex;
+    gap: 40px;
+  }
+  form {
+    padding: 18px;
+    border-radius: 20px;
+    border: ridge 1px;
+    box-shadow: 1px 1px 1px 1px gray;
+    font-weight: bold;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    justify-content: center;
+  }
+  form div {
+    display: grid;
+    grid-template-columns: 100px 200px;
+    align-items: center;
+    gap: 5px;
+  }
+
+  form div label {
+    text-align: right;
+  }
+
+  .carnet {
+    width: 400px;
+    height: 230px;
+    background: #1c1a1e;
+    color: white;
+    border-radius: 20px;
+    padding: 20px;
+    box-shadow: 0px 0px 10px 2px black;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  h3 {
+    text-align: left;
+    font-size: 20px;
+  }
+
+  h2 {
+    text-align: left;
+    font-size: 30px;
+    letter-spacing: 10px;
+    margin-bottom: 15px;
+    text-shadow: 0px 0px 1px gold;
+    font-family: Times;
+  }
+.datos{
+    text-align: left;
+}
+  b {
+    font-size: 16px;
+  }
+
+  footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    font-weight: bold;
+    font-size: 18px;
+    text-transform: uppercase;
+    font-style: italic;
+  }
+</style>
+  
